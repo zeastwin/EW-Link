51e68df41a8d82dd25369d3ddab01fd186214e28
zeastwin <zeastwin@sina.com>
2025-12-17 17:25:29 +0800

    first commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EW_Link.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
